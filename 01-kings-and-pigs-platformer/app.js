fd27eaf815d69f7dca5dd323d5bf745fd078b3e8
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-12-11 00:12:14 +0200

    🚀[Feature]: create 'player' class

--- 01-kings-and-pigs-platformer/app.js
+++ 01-kings-and-pigs-platformer/app.js
@@ -4,25 +4,41 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16; // 1024
 canvas.height = 64 * 9; // 576
 
-// initial Y coordinate position
-let y = 100;
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.width = 100;
+    this.height = 100;
+    this.sides = {
+      bottom: this.position.y + this.height,
+    };
+  }
+  draw() {
+    // draw the rectangle and move its Y coordinate
+    c.fillStyle = "#F00";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  update() {
+    if (this.sides.bottom < canvas.height) {
+      this.position.y++;
+      this.sides.bottom = this.position.y + this.height;
+    }
+  }
+}
 
-const height = 100;
-let bottom = y + 100;
+const player = new Player();
+
 function animate() {
   window.requestAnimationFrame(animate);
   // draw canvas background
   c.fillStyle = "#fff";
   c.fillRect(0, 0, canvas.width, canvas.height);
 
-  // draw the rectangle and move its Y coordinate
-  c.fillStyle = "#F00";
-  c.fillRect(100, y, 100, 100);
-
-  if (bottom < canvas.height) {
-    y++;
-    bottom = y + 100;
-  }
+  player.draw();
+  player.update();
 }
 
 animate();
