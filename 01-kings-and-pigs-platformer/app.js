b4cbee81e9220396c132bc38d01fd3fbcabb504a
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-12-10 23:43:46 +0200

    🚀[Feature]: create animate function
    Created animate function to handle the Y axis translate

--- 01-kings-and-pigs-platformer/app.js
+++ 01-kings-and-pigs-platformer/app.js
@@ -4,8 +4,18 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16; // 1024
 canvas.height = 64 * 9; // 576
 
-c.fillStyle = "#fff";
-c.fillRect(0, 0, canvas.width, canvas.height);
+// initial Y coordinate position
+let y = 100;
+function animate() {
+  window.requestAnimationFrame(animate);
+  // draw canvas background
+  c.fillStyle = "#fff";
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
-c.fillStyle = "#F00";
-c.fillRect(100, 100, 100, 100);
+  // draw the rectangle and move its Y coordinate
+  c.fillStyle = "#F00";
+  c.fillRect(100, y, 100, 100);
+  y++;
+}
+
+animate();
