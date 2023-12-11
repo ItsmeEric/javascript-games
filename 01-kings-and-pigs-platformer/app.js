0ad58b524d82a286aa9590ba1a65215f1f5c5b78
Eric Mbarushimana <mbarushimanaeric17@gmail.com>
2023-12-11 16:22:08 +0200

    🚀[feat]: add x axis movement to the player object

--- 01-kings-and-pigs-platformer/app.js
+++ 01-kings-and-pigs-platformer/app.js
@@ -6,11 +6,30 @@ canvas.height = 64 * 9; // 576
 
 const player = new Player();
 
+const keys = {
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  w: {
+    pressed: false,
+  },
+};
+
 function animate() {
   window.requestAnimationFrame(animate);
   // draw canvas background
   c.fillStyle = "#fff";
   c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.velocity.x = 0;
+  if (keys.a.pressed) player.velocity.x = -5;
+  else if (keys.d.pressed) player.velocity.x = 5;
 
   player.draw();
   player.update();
